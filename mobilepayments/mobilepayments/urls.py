3a9389a3fdad7be44a8885738da3fadee37bf549
ivanbok <bokivan@hotmail.com>
2020-12-11 11:20:08 +0800

    implement login

--- mobilepayments/mobilepayments/urls.py
+++ mobilepayments/mobilepayments/urls.py
@@ -1,4 +1,4 @@
-"""mobilepayments URL Configuration
+"""api_example URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('languages.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view())
 ]
--- mobilepayments/paymentapp/views.py
+++ mobilepayments/paymentapp/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Language, Paradigm, Programmer
+from .serializers import LanguageSerializer, ParadigmSerializer, ProgrammerSerializer
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+
+
+# Create your views here.
+class LanguageView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+class ParadigmView(viewsets.ModelViewSet):
+    queryset = Paradigm.objects.all()
+    serializer_class = ParadigmSerializer
+
+
+class ProgrammerView(viewsets.ModelViewSet):
+    queryset = Programmer.objects.all()
+    serializer_class = ProgrammerSerializer
+
+def index(request):
+    return render(request, 'paymentapp/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else: 
+        form = UserCreationForm()
+    
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
