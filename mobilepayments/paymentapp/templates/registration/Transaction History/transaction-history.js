fcacc9998029a65b48b58125a945fe99b6c2188a
Wei Ming <weiming.soh.2017@sis.smu.edu.sg>
2020-12-11 15:58:54 +0800

    Transaction History View
    Call to API to get transaction view.

--- mobilepayments/paymentapp/templates/registration/Transaction History/transaction-history.js
+++ mobilepayments/paymentapp/templates/registration/Transaction History/transaction-history.js
@@ -1,36 +1,24 @@
-blockchainViewRouter.post('/')
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+var raw = JSON.stringify({"custID":24});
+
+      var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+      };
+
+      fetch("https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/transaction/view", requestOptions)
+      .then(response => response.text())
+      .then(function(result) {
+        var obj = JSON.parse(result);
+        document.getElementById("cur-balance-details").innerHTML = "Balance in the account is $" + obj[0].availableBal;
+        document.getElementById("multi-balance-details").innerHTML = "Balance in the account is $" + obj[1].availableBal;
+        document.getElementById("savings-balance-details").innerHTML = "Balance in the account is $" + obj[2].availableBal;
+      })
+      .catch(error => console.log('error', error));
+
+const transferButton = document.getElementById("transfer-form-submit");
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-
-
-    var myHeaders = new Headers();
-    myHeaders.append("x-api-key", "WaiaCA14j19ibdk6JZR3D5E03Y8I21HQ3nDMh3c5");
-    myHeaders.append("Content-Type", "text/plain");
-
-    var raw = "{\n    \"username\": \"Group24\",\n    \"password\": \"EXDv_Kuy_5hKhxD\"\n}";
-
-    var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
-    };
-
-    fetch("https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/history", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
+    location.replace(".Tranfer/transfer.html");
 })
