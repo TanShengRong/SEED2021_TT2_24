0173ae2a2f003b07afacd5f4c50fc7ac5eb30d24
raumh <69139382+raumh@users.noreply.github.com>
2020-12-11 11:27:23 +0800

    creating database and user class

--- API/user_credentials/user_credentials.py
+++ API/user_credentials/user_credentials.py
@@ -6,3 +6,18 @@ import jwt
 import datetime
 from functools import wraps
 
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'thisissecret'
+#create a database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./local.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    #public id will be seen when decoded, so make it different from the id cause then itll be harder for people to figure out the users in the database
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean) #will be true or false, whether they are admin or not 
