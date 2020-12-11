c6fcee9eb7aa7a2e746f7475a688c1ffab1cd65f
TanShengRong <tansr2010@gmail.com>
2020-12-11 15:40:54 +0800

    quick fix transaction
    remove rel to account.js

--- mobilepayments/paymentapp/templates/registration/Account/account.js
+++ mobilepayments/paymentapp/templates/registration/Account/account.js
@@ -1,4 +1,24 @@
+var raw = JSON.stringify({"custID":7});
+
+      var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+      };
+
+      fetch("https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view", requestOptions)
+      .then(response => response.text())
+      .then(function(result) {
+        var obj = JSON.parse(result);
+        document.getElementById("cur-balance-details").innerHTML = "Balance in the account is $" + obj[0].availableBal;
+        document.getElementById("multi-balance-details").innerHTML = "Balance in the account is $" + obj[1].availableBal;
+        document.getElementById("savings-balance-details").innerHTML = "Balance in the account is $" + obj[2].availableBal;
+      })
+      .catch(error => console.log('error', error));
+
 const transferButton = document.getElementById("transfer-form-submit");
 
 loginButton.addEventListener("click", (e) => {
     location.replace(".Tranfer/transfer.html");
+})
