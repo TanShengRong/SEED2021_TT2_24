559704e876b0cc67d8425557e44c49ec07fe4cd0
Wenhui <khamwenhui.work@gmail.com>
2020-12-11 11:41:00 +0800

    AccountInfo

---
+++ API/Data/Migrations/20201211034037_AccountInfos.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class AccountInfos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccountInfos",
+                columns: table => new
+                {
+                    custID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    accountName = table.Column<string>(type: "TEXT", nullable: true),
+                    accountNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    availableBal = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountInfos", x => x.custID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LoginCredentials",
+                columns: table => new
+                {
+                    custID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    username = table.Column<string>(type: "TEXT", nullable: true),
+                    password = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoginCredentials", x => x.custID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountInfos");
+
+            migrationBuilder.DropTable(
+                name: "LoginCredentials");
+        }
+    }
+}
