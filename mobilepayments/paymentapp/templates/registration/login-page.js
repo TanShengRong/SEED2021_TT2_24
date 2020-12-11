b279aadda4d4535620959f2bff2a294a42f3ce93
Wei Ming <weiming.soh.2017@sis.smu.edu.sg>
2020-12-11 14:10:13 +0800

    Update login-page
    API Call for login still fail.

--- mobilepayments/paymentapp/templates/registration/login-page.js
+++ mobilepayments/paymentapp/templates/registration/login-page.js
@@ -8,18 +8,24 @@ loginButton.addEventListener("click", (e) => {
     const password = loginForm.password.value;
 
     url = 'https://cors-anywhere.herokuapp.com/https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/login'
-    fetch(url).then(response => {
+    fetch(url, {
+        method: "POST",
+        headers: {
+            'x-api-key': "WaiaCA14j19ibdk6JZR3D5E03Y8I21HQ3nDMh3c5",
+        },
+        body: JSON.stringify({
+            custID: 7,
+        })
+    }).then(response => {
         return response.json();
     })
     .then(login=>{
         console.log(login);
     })
-    
 
-
-    if (username === "user" && password === "web_dev") {
+    if (username === login.user && password === login.password) {
         alert("You have successfully logged in.");
-        location.reload();
+        location.replace("./balance.html");
     } else {
         loginErrorMsg.style.opacity = 1;
     }
