ee75be130a56fe0d081fb67cb87e28de0110e71b
raumh <69139382+raumh@users.noreply.github.com>
2020-12-11 11:33:07 +0800

    updating user class

--- API/user_credentials/user_credentials.py
+++ API/user_credentials/user_credentials.py
@@ -15,9 +15,32 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./local.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    custID = db.Column(db.Integer, primary_key=True)
     #public id will be seen when decoded, so make it different from the id cause then itll be harder for people to figure out the users in the database
     public_id = db.Column(db.String(50), unique=True)
-    name = db.Column(db.String(50))
-    password = db.Column(db.String(80))
-    admin = db.Column(db.Boolean) #will be true or false, whether they are admin or not 
+    firstname = db.Column(db.String(50))
+    lastname = db.Column(db.String(50))
+    nric = db.Column(db.String(10))
+    gender = db.Column(db.String(10))
+    age = db.Column(db.Integer(3))
+    phoneNumber = db.Column(db.String(10))
+    email = db.Column(db.String(80))
+    address = db.Column(db.String(80))
+
+app.route('/user', methods=['GET'])
+@token_required
+def get_all_users():
+
+    users = User.query.all()
+
+    output = [] #need to generate a new list first because cannot output sqlalchemy results into a json object directly
+
+    for user in users:
+        user_data = {} #create new dictionary for each user
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password #it will be hashed 
+        user_data['admin'] = user.admin
+        output.append(user_data)    
+
+    return jsonify({'users' : output})
